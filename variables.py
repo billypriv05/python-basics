132efa93f0aa4bc0e071ab04d5909f316f33272f
billypriv05 <93233789+billypriv05@users.noreply.github.com>
2021-10-27 20:04:40 +1300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+# code to learn variables
+
+# variables can be a string
+greeting = "hello world"
+
+# they can also be numbers
+fav_number = 7
+
+# Below is a constant (the name is in UPPERCASE)
+PI = 3.14
+
+num_2 = 2
